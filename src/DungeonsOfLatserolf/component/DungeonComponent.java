daeb1e11ed6da40f6580312e5376e6b609ce9a98
Gabriel R. Marques <gabriel.r.marques@ufv.br>
2023-11-05 14:32:31 -0300

    Update DungeonComponent Tiles

--- src/DungeonsOfLatserolf/component/DungeonComponent.java
+++ src/DungeonsOfLatserolf/component/DungeonComponent.java
@@ -2,28 +2,34 @@ package DungeonsOfLatserolf.component;
 
 import java.util.Random;
 
-import DungeonsOfLatserolf.Main;
-
 public class DungeonComponent {
+
     private static final int WIDTH = 31;
     private static final int HEIGHT = 31;
-    private static final double DOOR_PROBABILITY = 0.1;
+    // private static final double DOOR_PROBABILITY = 0.1;
     private static final double MONSTER_PROBABILITY = 0.1;
     private static final double CHEST_PROBABILITY = 0.01;
     private static final char BOARD = 'O';
     private static final char WALL = '#';
+    private static final char vWALL = '║';
+    private static final char hWALL = '═';
     private static final char FLOOR = '.';
-    private static final char DOOR = 'D';
+    // private static final char DOOR = 'D';
     private static final char CHEST = 'C';
     private static final char MONSTER = 'M';
     private static final char START = 'S';
-    private static final char EXIT = 'E';
+    public static final int START_X = WIDTH / 2;
+    private static final int START_Y = HEIGHT / 2;
+
     private char DungeonComponent[][];
 
     public DungeonComponent() {
         this.DungeonComponent = new char[WIDTH][HEIGHT];
+        generateDungeonComponent(this.DungeonComponent, START_X, START_Y);
+
     }
 
+    // REMOVER - Será usado somente para testes...
     public void showDungeonComponent() {
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < HEIGHT; y++) {
@@ -33,29 +39,54 @@ public class DungeonComponent {
         }
     }
 
-    public void generateDungeonComponent() {
-        Random random = new Random();
+    private void generateDungeonComponent(char[][] DungeonComponent, int startX, int startY) {
+        buildDungeonComponent(DungeonComponent);
+        buildBacktrackingDungeonComponent(DungeonComponent, START_X, START_Y);
+        buildDoorDungeonComponent(DungeonComponent);
+        buildWallTileDungeonComponent(DungeonComponent);
+    }
 
-        int startX = WIDTH / 2;
-        int startY = HEIGHT / 2;
-        System.out.println(startX);
-        System.out.println(startY);
-
-        int exitX = random.nextInt(WIDTH - 2) + 1;
-        int exitY = random.nextInt(WIDTH - 2) + 1;
-
+    private void buildDungeonComponent(char[][] DungeonComponent) {
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < HEIGHT; y++) {
                 if (x == 0 || x == WIDTH - 1 || y == 0 || y == HEIGHT - 1) {
-                    this.DungeonComponent[x][y] = BOARD;
+                    DungeonComponent[x][y] = BOARD;
                 } else {
-                    this.DungeonComponent[x][y] = WALL;
+                    DungeonComponent[x][y] = WALL;
                 }
             }
         }
-        
-        backtrackingDungeonComponent(this.DungeonComponent, startX, startY);
+        DungeonComponent[START_X][START_Y] = START;
+    }
 
+    private void buildBacktrackingDungeonComponent(char[][] DungeonComponent, int startX, int startY) {
+        int[] dx = { 1, -1, 0, 0 };
+        int[] dy = { 0, 0, 1, -1 };
+        int[] directions = { 0, 1, 2, 3 };
+        shuffleDirections(directions);
+        for (int dir : directions) {
+            int nx = startX + dx[dir] * 2;
+            int ny = startY + dy[dir] * 2;
+            if (nx >= 0 && nx < DungeonComponent[0].length && ny >= 0 && ny < DungeonComponent[0].length
+                    && DungeonComponent[nx][ny] == WALL) {
+                DungeonComponent[startX + dx[dir]][startY + dy[dir]] = FLOOR;
+                DungeonComponent[nx][ny] = FLOOR;
+                buildBacktrackingDungeonComponent(DungeonComponent, nx, ny);
+            }
+        }
+    }
+
+    private void shuffleDirections(int[] directions) {
+        Random random = new Random();
+        for (int i = directions.length - 1; i > 0; i--) {
+            int index = random.nextInt(i + 1);
+            int temp = directions[index];
+            directions[index] = directions[i];
+            directions[i] = temp;
+        }
+    }
+
+    private void buildDoorDungeonComponent(char[][] DungeonComponent) {
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < HEIGHT; y++) {
                 if (DungeonComponent[x][y] == FLOOR) {
@@ -70,33 +101,25 @@ public class DungeonComponent {
                 }
             }
         }
-
-        DungeonComponent[startX][startY] = START;
     }
 
-    private void backtrackingDungeonComponent(char[][] DungeonComponent, int startX, int startY) {
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        int[] directions = {0, 1, 2, 3};
-        shuffleDirections(directions);
-        for (int dir : directions) {
-            int nx = startX + dx[dir] * 2;
-            int ny = startY + dy[dir] * 2;
-            if (nx >= 0 && nx < DungeonComponent[0].length && ny >= 0 && ny < DungeonComponent[0].length && DungeonComponent[nx][ny] == WALL) {
-                DungeonComponent[startX + dx[dir]][startY + dy[dir]] = FLOOR;
-                DungeonComponent[nx][ny] = FLOOR;
-                backtrackingDungeonComponent(DungeonComponent, nx, ny);
+    private void buildWallTileDungeonComponent(char[][] DungeonComponent) {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (DungeonComponent[x][y] == WALL) {
+                    if (DungeonComponent[x + 1][y] == WALL)
+                        DungeonComponent[x][y] = vWALL;
+                    if (DungeonComponent[x - 1][y] == WALL)
+                        DungeonComponent[x][y] = vWALL;
+                }
             }
         }
-    }
-
-    private static void shuffleDirections(int[] directions) {
-        Random random = new Random();
-        for (int i = directions.length - 1; i > 0; i--) {
-            int index = random.nextInt(i + 1);
-            int temp = directions[index];
-            directions[index] = directions[i];
-            directions[i] = temp;
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (DungeonComponent[x][y] == WALL) {
+                    DungeonComponent[x][y] = hWALL;
+                }
+            }
         }
     }
 }
--- src/Test/TestDungeonComponent.java
+++ src/Test/TestDungeonComponent.java
@@ -5,7 +5,6 @@ import DungeonsOfLatserolf.component.DungeonComponent;
 public class TestDungeonComponent {
     public static void main(String[] args) {
         DungeonComponent dungeonComponent = new DungeonComponent();
-        dungeonComponent.generateDungeonComponent();
         dungeonComponent.showDungeonComponent();
     }
 }
