80802cda4a2dbd58bb8646bce5ac273ed2d1e6b2
Arthur-Ataide <arthursaraiva0704@gmail.com>
2023-11-09 09:25:40 -0300

    monster

--- src/DungeonsOfLatserolf/entiry/Seres/Player.java
+++ src/DungeonsOfLatserolf/entiry/Seres/Player.java
@@ -1,6 +1,7 @@
-package DungeonsOfLatserolf;
+package DungeonsOfLatserolf.entiry.Seres;
 
 public class Player {
+    private String name;
     private int health;
     private int attack;
     private int keysCollected;
