b8bbe3d542e9ee9b4693395c7cb2fa3812f4f580
Gabriel R. Marques <gabriel.r.marques@ufv.br>
2023-11-10 00:36:11 -0300

    database

--- src/DungeonsOfLatserolf/DatabaseManagerSystem.java
+++ src/DungeonsOfLatserolf/DatabaseManagerSystem.java
@@ -3,22 +3,28 @@ package DungeonsOfLatserolf;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.mysql.cj.xdevapi.PreparableStatement;
-
 public class DatabaseManagerSystem {
-    private static final String url = "jdbc:mysql://localhost:3306/DungeonsOfLatserolf";
-    private static final String user = "root";
-    private static final String password = "r4@P36~y_buT";
+    private static final String URL = "jdbc:mysql://localhost:3306/DungeonsOfLatserolf";
+    private static final String USER = "root";
+    private static final String PASSWORD = "r4@P36~y_buT";
+
     private static Connection connection;
+    
+    private PlayerEntity playerEntity;
 
     private static final String SQL_INSERT_NEW_USER = "INSERT INTO TBPlayer (playerEmail, playerName, playerScore) VALUES (?, ?, ?)";
+    private static final String SQL_SELECT_USER_BY_EMAIL = "SELECT * FROM TBPlayer WHERE playerEmail = ?";
+    private static final String SQL_SELECT_ALL_USERS = "SELECT * FROM TBPlayer";
+    private static final String SQL_UPDATE_USER_SCORE = "UPDATE TBPlayer SET playerScore = ? WHERE playerEmail = ?";
+    private static final String SQL_DELETE_DATABASE = "DELETE FROM TBPlayer";
 
     public static Connection getConnection() {
         try {
             if (connection == null) {
-                connection = DriverManager.getConnection(url, user, password);
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
                 return connection;
             } else {
                 return connection;
@@ -44,10 +50,33 @@ public class DatabaseManagerSystem {
             preparedStatement.setString(1, playerEntity.getPlayerEmail());
             preparedStatement.setString(2, playerEntity.getPlayerName());
             preparedStatement.setInt(3, playerEntity.getPlayerScore());
-            preparedStatement.executeUpdate();
+            preparedStatement.execute();
             preparedStatement.close();
         } catch (SQLException exception) {
             exception.printStackTrace();
+        }
+    }
+
+    public String userLogin(String playerEmail) {
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        String result = null;
+        try {
+            preparedStatement = getConnection().prepareStatement(SQL_SELECT_USER_BY_EMAIL);
+            preparedStatement.setString(1, playerEmail);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                result = resultSet.getString("playerName") + " ";
+                result += resultSet.getInt("playerScore");
+            } else {
+                result = "User not found";
+            }
+            resultSet.close();
+            preparedStatement.close();
+            return result;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
         }
     }
 }
--- src/TestDungeonsOfLatserolf/TestDatabaseManagerSystem.java
+++ src/TestDungeonsOfLatserolf/TestDatabaseManagerSystem.java
@@ -1,12 +1,29 @@
 package TestDungeonsOfLatserolf;
 
+import java.util.Scanner;
+
 import DungeonsOfLatserolf.DatabaseManagerSystem;
 import DungeonsOfLatserolf.PlayerEntity;
 
 public class TestDatabaseManagerSystem {
     public static void main(String[] args) {
-        PlayerEntity playerEntity = new PlayerEntity("gabriel.r.marques@ufv.br", "dulol", 0);
+        // Cria um novo usuário
+        Scanner sc = new Scanner(System.in);
+        System.out.println("CADASTRANDO");
+        System.out.println("E-mail : ");
+        String playerEmail = sc.nextLine();
+        System.out.println("Nome : ");
+        String playerName = sc.nextLine();
+        System.out.println("Score : ");
+        int playerScore = sc.nextInt();
+        PlayerEntity playerEntity = new PlayerEntity(playerEmail, playerName, playerScore);
         new DatabaseManagerSystem().newUser(playerEntity);
-        
+        // Consulta todos os usuários cadastrados
+        System.out.println("LOGIN");
+        System.out.println("E-mail : ");
+        sc.nextLine();
+        playerEmail = sc.nextLine();
+        sc.close();
+        System.out.println(new DatabaseManagerSystem().userLogin(playerEmail));
     }
 }
