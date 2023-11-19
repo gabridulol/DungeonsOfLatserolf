a4f2ec7aca6eb9ca7307cbbbc4b8de5d2cc4d531
Gabriel R. Marques <gabriel.r.marques@ufv.br>
2023-11-19 13:43:00 -0300

    AssetLibraryComponent

---
+++ src/TestDungeonsOfLatserolf/TestAssetLibrary.java
@@ -0,0 +1,10 @@
+package TestDungeonsOfLatserolf;
+
+import DungeonsOfLatserolf.component.AssetLibraryComponent;
+
+public class TestAssetLibrary {
+    public static void main(String[] args) {
+        AssetLibraryComponent assetLibrary = new AssetLibraryComponent();
+        assetLibrary.printHashMap();
+    }
+}
